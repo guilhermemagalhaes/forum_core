76a23d2942f4856da53794d930038a5eb12cc6a3
Guilherme Magalhães <guilherme19988@hotmail.com>
2020-05-28 16:15:19 -0300

    Arquitetura inicial

---
+++ forum.core/Models/Comentario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace forum.core.Models
+{
+    public class Comentario
+    {
+        [Key]
+        public int ComentarioId { get; set; }
+        public string Autor { get; set; }
+        public string Texto { get; set; }
+        public DateTime DataCadastro { get; set; }        
+        [ForeignKey("Post")]
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+    }
+}
---
+++ forum.core/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace forum.core.Models
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        public string Assunto { get; set; }
+        public string Texto { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataAtualizacao { get; set; }
+        public bool FlaAtivo { get; set; }
+        public string Autor { get; set; }
+        public ICollection<Comentario> Comentarios { get; set; }
+    }
+}
